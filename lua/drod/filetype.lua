fe49c3d4a693a14171e5a6a433fec031dd620ced
Rodriguez, Daniel T <daniel.t.rodriguez@intel.com>
2023-04-26 00:52:49 -0700

    Fixed syntax errors in filetype

--- lua/drod/filetype.lua
+++ lua/drod/filetype.lua
@@ -1,8 +1,8 @@
 vim.filetype.add({
     extension = {
         opt = 'perl',
-        sv09 = systemverilog
-        vg = verilog
+        sv09 = systemverilog,
+        vg = verilog,
         ['do'] = 'tcl',
         -- TODO: Make ICL/PDL ft plugin
         pdl = 'tcl',
