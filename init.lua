cf344c4182118e3ff7ec97c81b8dbc230d96e8a5
Rodriguez, Daniel T <daniel.t.rodriguez@intel.com>
2023-05-09 21:33:33 -0700

    intel specific configs separate from my general profile

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
+-- For all nvim settings that are personal
 require("drod")
+-- For all nvim settings that are specific to Intel workflows
+require("intel")
---
+++ lua/intel/filetype.lua
@@ -0,0 +1,13 @@
+vim.filetype.add({
+    extension = {
+        opt = 'perl', -- legacy ace stuff
+        ['do'] = 'tcl',
+        pdl = 'tcl',-- TODO: Make ICL/PDL ft plugin
+        f = 'nosyntax', -- Filelists
+        cth = 'ini',
+    },
+    pattern = {
+        -- Matching model config files
+        [".*verif/[%w_]+/[%w_]+.cfg"] = 'yaml',
+    }
+})
